6dd71e294c2a96b5ad4a548c1826f9e180c1a77d
hppeng <hppeng>
2026-02-25 20:21:25 +0000

    Script for downloading lora weights
    untested

---
+++ download_lora_weights.sh
@@ -0,0 +1,10 @@
+SCRIPT_DIR=$(cd -- "$(dirname -- "$BASH_SOURCE[0]")/" && pwd)
+
+cd "$SCRIPT_DIR/.."
+mkdir -p checkpoints/pi05_libero_reason_lora
+cd checkpoints/pi05_libero_reason_lora
+git clone https://huggingface.co/n5zhong/pi05_libero_reason_lora.git
+cd "$SCRIPT_DIR"
+mkdir -p assets/pi05_libero_reason_lora
+cd assets/pi05_libero_reason_lora
+ln -sT ../../checkpoints/pi05_libero_reason_lora/pi05_libero_reason_lora/31799/assets/yilin-wu/
