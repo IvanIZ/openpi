ea14267eec3845a60d0a722a4745f875abd39751
Ninghan Zhong <zhong2@gh-login01.delta.ncsa.illinois.edu>
2026-05-09 18:30:40 -0500

    remove subtask sampling for pi0 class

--- src/openpi/models/pi0.py
+++ src/openpi/models/pi0.py
@@ -410,6 +410,29 @@ class Pi0(_model.BaseModel):
         return x_0
 
 
+    # @override
+    # def sample_actions(
+    #     self,
+    #     rng: at.KeyArrayLike,
+    #     observation: _model.Observation,
+    #     *,
+    #     num_steps: int | at.Int[at.Array, ""] = 10,
+    #     noise: at.Float[at.Array, "b ah ad"] | None = None,
+    # ) -> _model.Actions:
+    #     observation = _model.preprocess_observation(None, observation, train=False)
+    #     # note that we use the convention more common in diffusion literature, where t=1 is noise and t=0 is the target
+    #     # distribution. yes, this is the opposite of the pi0 paper, and I'm sorry.
+    #     dt = -1.0 / num_steps
+    #     batch_size = observation.state.shape[0]
+	# 	# TODO: Can cause issues since subtask lengths are different, leading to more tokens past EOS if batch size is not 1.
+    #     if noise is None:
+    #         noise = jax.random.normal(rng, (batch_size, self.action_horizon, self.action_dim))
+
+    #     # first fill KV cache with a forward pass of the prefix
+    #     output_tokens, kv_cache, prefix_mask, prefix_ar_mask = self.sample_low_level_task(rng, observation, max_decoding_steps=20, temperature=0.0)
+    #     return (self._generate_action(noise, dt, kv_cache, prefix_mask, batch_size), output_tokens)
+
+
     @override
     def sample_actions(
         self,
@@ -424,10 +447,53 @@ class Pi0(_model.BaseModel):
         # distribution. yes, this is the opposite of the pi0 paper, and I'm sorry.
         dt = -1.0 / num_steps
         batch_size = observation.state.shape[0]
-		# TODO: Can cause issues since subtask lengths are different, leading to more tokens past EOS if batch size is not 1.
         if noise is None:
             noise = jax.random.normal(rng, (batch_size, self.action_horizon, self.action_dim))
 
         # first fill KV cache with a forward pass of the prefix
-        output_tokens, kv_cache, prefix_mask, prefix_ar_mask = self.sample_low_level_task(rng, observation, max_decoding_steps=20, temperature=0.0)
-        return (self._generate_action(noise, dt, kv_cache, prefix_mask, batch_size), output_tokens)
+        prefix_tokens, prefix_mask, prefix_ar_mask = self.embed_prefix(observation)
+        prefix_attn_mask = make_attn_mask(prefix_mask, prefix_ar_mask)
+        positions = jnp.cumsum(prefix_mask, axis=1) - 1
+        _, kv_cache, _ = self.PaliGemma.llm([prefix_tokens, None], mask=prefix_attn_mask, positions=positions)
+
+        def step(carry):
+            x_t, time = carry
+            suffix_tokens, suffix_mask, suffix_ar_mask, adarms_cond = self.embed_suffix(
+                observation, x_t, jnp.broadcast_to(time, batch_size)
+            )
+            # `suffix_attn_mask` is shape (b, suffix_len, suffix_len) indicating how the suffix tokens can attend to each
+            # other
+            suffix_attn_mask = make_attn_mask(suffix_mask, suffix_ar_mask)
+            # `prefix_attn_mask` is shape (b, suffix_len, prefix_len) indicating how the suffix tokens can attend to the
+            # prefix tokens
+            prefix_attn_mask = einops.repeat(prefix_mask, "b p -> b s p", s=suffix_tokens.shape[1])
+            # `combined_mask` is shape (b, suffix_len, prefix_len + suffix_len) indicating how the suffix tokens (which
+            # generate the queries) can attend to the full prefix + suffix sequence (which generates the keys and values)
+            full_attn_mask = jnp.concatenate([prefix_attn_mask, suffix_attn_mask], axis=-1)
+            assert full_attn_mask.shape == (
+                batch_size,
+                suffix_tokens.shape[1],
+                prefix_tokens.shape[1] + suffix_tokens.shape[1],
+            )
+            # `positions` is shape (b, suffix_len) indicating the positions of the suffix tokens
+            positions = jnp.sum(prefix_mask, axis=-1)[:, None] + jnp.cumsum(suffix_mask, axis=-1) - 1
+
+            (prefix_out, suffix_out), _, _ = self.PaliGemma.llm(
+                [None, suffix_tokens],
+                mask=full_attn_mask,
+                positions=positions,
+                kv_cache=kv_cache,
+                adarms_cond=[None, adarms_cond],
+            )
+            assert prefix_out is None
+            v_t = self.action_out_proj(suffix_out[:, -self.action_horizon :])
+
+            return x_t + dt * v_t, time + dt
+
+        def cond(carry):
+            x_t, time = carry
+            # robust to floating-point error
+            return time >= -dt / 2
+
+        x_0, _ = jax.lax.while_loop(cond, step, (noise, 1.0))
+        return x_0
